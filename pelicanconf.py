f92a132e4e825000c224bbc8376071467e191259
Sushant Srivastava <sushant.srivastav@gmail.com>
2014-12-13 01:00:04 +0530

    Adding the blog post to vcs

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,10 +5,10 @@ from __future__ import unicode_literals
 AUTHOR = u'Sushant'
 SITENAME = u'orom'
 SITEURL = ''
+DISQUS_SITENAME='oromin'
 
 PATH = 'content'
-
-TIMEZONE = 'Europe/Paris'
+PLUGINS = ['series']
 
 DEFAULT_LANG = u'en'
 
@@ -16,18 +16,16 @@ DEFAULT_LANG = u'en'
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
-THEME='/Users/susrivas/pelican-themes/notebook'
-
+THEME='/Users/susrivas/pelican-themes/tuxlite_tbs'
+PLUGIN_PATHS=["plugins" , "/Users/susrivas/pelican-plugins"]
+SOCIAL = (('twitter', 'http://twitter.com/ssushant'),
+          ('github', 'http://github.com/mitra-varuna'))
+CHECK_MODIFIED_METHOD='mtime'
+GOOGLE_ANALYTICS='UA-57632501-1'
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = False
 
